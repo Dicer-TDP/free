dea0db9afbd2ade31e370c9f3ab8111c5372d8e2
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2020-01-05 13:07:04 +0700

    Add files via upload

---
+++ Script Hack Kamera Android (Depan).sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+
+# Daftar Warna :
+# ---------------------------------------------
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+nc='\e[0m'
+
+# Efek Tulisan
+1='\e[0;00m' # Reset standar
+2='\e[1;00m' # Efek warna agak terang
+3='\e[2;00m' # Efek warna agak gelap
+4='\e[3;00m' # Efek warna agak gelap
+5='\e[4;00m' # Garis Bawah
+6='\e[5;00m' # Efek kedap-kedip
+7='\e[6;00m' # Hasilnya sama dengan 2 dan 3
+8='\e[7;00m' # Warna dijadikan latar belakang teks
+9='\e[8;00m' # Tidak dikenal [range of 8-29]
+10='\e[30;00m' # Hitam tipis/gelap
+# Komentar Kosong
+
+echo -e"{$lightblue}Created{$lightgreen}by: ";
+echo -e"{$white}Dicer-TDP ";
+echo -e"Write up at: 2020-01-05";
+echo -e"Special thanks: Contibutors and Termux Discuss Party ";
+
+sleep 200
+
+# Efek dash eksekusi
+
